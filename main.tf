3874a79f810fed97988c100f0982241586e8fd67
Jairo <hardtelles@gmail.com>
2024-02-23 06:30:01 -0300

    libera porta 8082 subrede

--- main.tf
+++ main.tf
@@ -101,6 +101,13 @@ resource "aws_security_group" "security_group_fasteats" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 8082
+    to_port   = 8082
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
